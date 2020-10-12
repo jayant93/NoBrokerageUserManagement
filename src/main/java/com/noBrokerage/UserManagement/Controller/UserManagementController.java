2b7aebc0c81665c829b8f19ee1b1b428adc14c33
jayant93 <53552844+jayant93@users.noreply.github.com>
2020-10-12 14:24:01 +0530

    Update UserManagementController.java
    Follow for Reference

--- src/main/java/com/noBrokerage/UserManagement/Controller/UserManagementController.java
+++ src/main/java/com/noBrokerage/UserManagement/Controller/UserManagementController.java
@@ -1,18 +1,11 @@
 package com.noBrokerage.UserManagement.Controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.noBrokerage.UserManagement.Entity.UserAddress;
-import com.noBrokerage.UserManagement.Entity.UserDetails;
-import com.noBrokerage.UserManagement.Entity.UserRoles;
 import com.noBrokerage.UserManagement.Service.UserManagementService;
 
 @RestController
@@ -23,9 +16,11 @@ public class UserManagementController {
 	@Autowired
 	UserManagementService service;
 	
+	
+	//Code for testing the save feature
 	@GetMapping("/save")
 	public UserDetails saveUser(/*@RequestBody UserDetails user*/) {
-			//Code for testing the save feature
+			
 		
 		
 		
